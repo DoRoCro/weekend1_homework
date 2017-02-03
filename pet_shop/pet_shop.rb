d6a11884ab6ef867f788925d5900f800ab465427
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-03 10:43:36 +0000

    add total_cash function

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -3,3 +3,9 @@ def pet_shop_name(pet_shop_object)
   # pet_shop_object{ :name => string}
   return pet_shop_object[:name]
 end
+
+def total_cash(pet_shop_object)
+  # assumes structure in object passed in matches 
+  # pet_shop_object{ :admin => { total_cash: => integer}}
+  return pet_shop_object[:admin][:total_cash]
+end
