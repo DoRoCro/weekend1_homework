630a3108f4ad642dd05fb79248c6915fb13897f3
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-03 10:55:21 +0000

    Add add_or_remove_cash function tested for add

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -9,3 +9,11 @@ def total_cash(pet_shop_object)
   # pet_shop_object{ :admin => { total_cash: => integer}}
   return pet_shop_object[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop_object,cash)
+  # assumes structure in object passed in matches 
+  # pet_shop_object{ :admin => { total_cash: => integer}}
+  pet_shop_object[:admin][:total_cash] += cash
+  return pet_shop_object[:admin][:total_cash]
+end
+
