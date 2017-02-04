b53f8a51ca7453adfd8906fa98b426ee15fd755a
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-02-04 15:01:53 +0000

    add cash deduction from customer to sell_pet_to_customer as enhancement betond spec

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -1,7 +1,7 @@
-# assumed object structure
-#   pet_shop_object = {
+# assumed object structure for code below
+#   pet_shop = {
 #     :pets => [ {pet}, {pet} ...],
-#     :admin => { {} {} ...},
+#     :admin => { {:totalcash => int} {} ...},
 #     :name => string
 #     ...
 #   }
@@ -14,67 +14,67 @@
 #   }
 #   customer = {
 #     :name => string
-#     :pets => [ string, string, ... ]
+#     :pets => [ obj, obj, ... ]
 #     :case => integer
 #   }
 
-def pet_shop_name(pet_shop_object)
-  return pet_shop_object[:name]
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
 end
 
-def total_cash(pet_shop_object)
-  return pet_shop_object[:admin][:total_cash]
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(pet_shop_object,cash)
-  pet_shop_object[:admin][:total_cash] += cash
-  return pet_shop_object[:admin][:total_cash]
+def add_or_remove_cash(pet_shop,cash)
+  pet_shop[:admin][:total_cash] += cash
+  return pet_shop[:admin][:total_cash]
 end
 
-def pets_sold(pet_shop_object)
-  return pet_shop_object[:admin][:pets_sold]
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
 end
 
-def increase_pets_sold(pet_shop_object, num_pets)
-  pet_shop_object[:admin][:pets_sold] += num_pets
-  return pet_shop_object[:admin][:pets_sold]
+def increase_pets_sold(pet_shop, num_pets)
+  pet_shop[:admin][:pets_sold] += num_pets
+  return pet_shop[:admin][:pets_sold]
 end
 
-def stock_count(pet_shop_object)
-  # assumes each element of pet_shop_object[:pets] represents 1 pet
+def stock_count(pet_shop)
+  # assumes each element of pet_shop[:pets] represents 1 pet
   stock_count = 0
-  for pet in pet_shop_object[:pets]
+  for pet in pet_shop[:pets]
     stock_count += 1
   end
   return stock_count
 end
 
-def pets_by_breed(pet_shop_object, breed_name)
+def pets_by_breed(pet_shop, breed_name)
   pets_list = []
-  for pet in pet_shop_object[:pets]
+  for pet in pet_shop[:pets]
      pets_list.push(pet)  if (pet[:breed] == breed_name) 
   end
   return pets_list
 end
 
-def find_pet_by_name(pet_shop_object, pet_name)
-  # search through pet_shop for required pet, break out
+def find_pet_by_name(pet_shop, pet_name)
+  # search through pet_shop[:pets] for required pet, break out
   # of function on first match, else return nil
-  for pet in pet_shop_object[:pets]
+  for pet in pet_shop[:pets]
     return pet if ( pet[:name] == pet_name )
   end
   return
 end
 
-def remove_pet_by_name(pet_shop_object, pet_to_remove) 
+def remove_pet_by_name(pet_shop, pet_to_remove) 
   # DRY - use find instead of enumerating :pets array
-  return pet_shop_object[:pets].delete( 
-    find_pet_by_name(pet_shop_object, pet_to_remove)
+  return pet_shop[:pets].delete( 
+    find_pet_by_name(pet_shop, pet_to_remove)
     )
 end
 
-def add_pet_to_stock(pet_shop_object, new_pet)
-  return pet_shop_object[:pets].push(new_pet)
+def add_pet_to_stock(pet_shop, new_pet)
+  return pet_shop[:pets].push(new_pet)
 end
 
 def customer_pet_count(customer)
@@ -90,12 +90,14 @@ def customer_can_afford_pet(customer,pet)
   return ( customer[:cash] >= pet[:price] ) ? true : false  
 end
 
-def sell_pet_to_customer(pet_shop_object, pet, customer)
-  return if pet == nil
+def sell_pet_to_customer(pet_shop, pet, customer)
+  return false if pet == nil
   if customer_can_afford_pet(customer, pet)
     customer[:pets].push(pet)
-    pet_shop_object[:admin][:pets_sold] += 1
-    pet_shop_object[:admin][:total_cash] += pet[:price] 
+    pet_shop[:admin][:pets_sold] += 1
+    pet_shop[:admin][:total_cash] += pet[:price] 
+    # not required by test but seems a reasonable feature to include
+    customer[:cash] -= pet[:price]
   end
-  return
+  return true
 end
